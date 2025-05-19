05f3174c039cb9e85395c279357917d1d59ccc6f
Hippolyte Melica <hippolytemelica@gmail.com>
2025-05-19 17:45:49 +0200

    programme nul de debug, on va trouver la solution

--- src/params.c
+++ src/params.c
@@ -25,8 +25,7 @@ static const uint32_t write_tab[MAT_HEIGHT + 1] = {
     3 << 17, // value needed to set all to 0
 };
 
-static const uint32_t read_shift = 3;
-static const uint32_t read_mask = 0xF << read_shift;
+static const uint32_t read_mask  = (1 << 19 | 1 << 20 | 1 << 21);
 
 // static const uint32_t read_tab[MAT_WIDTH] = {
 //     3,
@@ -40,24 +39,37 @@ static const uint32_t read_mask = 0xF << read_shift;
 //     if (x >= MAT_WIDTH || y >= MAT_HEIGHT)
 //         return 0;
 //     if (!(LPC_GPIO0->FIOPIN & (1 << write_tab[y])))
-//         LPC_GPIO0->FIOPIN = (LPC_GPIO0->FIOPIN & ~(write_tab[MAT_HEIGHT] & (1 << write_tab[y]))) | (1 << write_tab[y]);
+//         LPC_GPIO0->FIOPIN = (LPC_GPIO0->FIOPIN & ~(write_tab[MAT_HEIGHT] & (1 << write_tab[y]))) | (1 <<
+//         write_tab[y]);
 //     return (LPC_GPIO0->FIOPIN & (1 << read_tab[x])) >> read_tab[x];
 // }
 
 uint8_t read_value(uint8_t y) {
-    LPC_GPIO0->FIOPIN = (LPC_GPIO0->FIOPIN | write_tab[MAT_HEIGHT]) & ~(1 << write_tab[y]);
-    return (LPC_GPIO0->FIOPIN & read_mask) >> read_shift;
+    LPC_GPIO0->FIOPIN  = (LPC_GPIO0->FIOPIN & ~write_tab[MAT_HEIGHT]) | 1 << write_tab[y];
+    uint32_t ret       = (((LPC_GPIO0->FIOPIN & (1 << 23)) >> 1) | (LPC_GPIO0->FIOPIN & read_mask)) >> 19;
+    LPC_GPIO0->FIOPIN &= ~write_tab[MAT_HEIGHT]; // to prevent pull down consumption
+    return ret;
 }
 
 void init_params() {
     // le pinsel est GPIO par défaut
     // le GPIO est en entrée par défaut
-    LPC_GPIO0->FIODIR |= 0x3 << 17; // 0.17 et 0.18 en sortie
+    printf("a\r\n");
+    LPC_PINCON->PINSEL1 &= ~((3 << 6) | (3 << 8) | (3 << 10) | (3 << 14));
+    LPC_GPIO0->FIODIR    |= (0x3 << 17); // 0.17 et 0.18 en sortie
+    LPC_GPIO0->FIODIR    &= ~(1 << 19 | 1 << 20 | 1 << 21 | 1 << 23); // en entrées
+    LPC_PINCON->PINMODE1 |= (3 << 6) | (3 << 8) | (3 << 10) | (3 << 14);
+    LPC_GPIO0->FIOPIN &= ~write_tab[MAT_HEIGHT]; // to prevent pull down consumption
+    printf("FIODIR : %08lx\r\n", LPC_GPIO0->FIODIR);
+    printf("FIOPIN : %08lx\r\n", LPC_GPIO0->FIOPIN);
+    printf("PINSEL : %08lx\r\n", LPC_PINCON->PINSEL1);
+    printf("PINMODE : %08lx\r\n", LPC_PINCON->PINMODE1);
+    printf("Init done\r\n");
 }
 
 void read_params() {
-    nb_robots = read_value(0);
-    nb_postes = read_value(1);
-    LPC_GPIO0->FIOPIN = (LPC_GPIO0->FIOPIN | write_tab[MAT_HEIGHT]); // to prevent pull up consumption
+    nb_robots          = read_value(0);
+    nb_postes          = read_value(1);
+    // LPC_GPIO0->FIOPIN &= ~write_tab[MAT_HEIGHT]; // to prevent pull down consumption
     printf("CFGR%02dP%02d\r\n", nb_robots, nb_postes);
 }
--- tools/clangd_generator.sh
+++ tools/clangd_generator.sh
@@ -1,14 +1,38 @@
 cat << EOF > .clangd
-CompileFlags:
-  Add:
-    - "-Wall"
-    - "-Wextra"
-    - "-I$(realpath LPC1769)"
-    - "-I$(realpath include)"
-    - "-ferror-limit=0"
-
-Diagnostics:
-  UnusedIncludes: Strict
+[
+  {
+    "arguments": [
+      "/usr/bin/arm-none-eabi-gcc",
+      "-mcpu=cortex-m3",
+      "-mthumb",
+      "-c",
+      "-fno-common",
+      "-fmessage-length=0",
+      "-Wall",
+      "-fno-exceptions",
+      "-ffunction-sections",
+      "-fdata-sections",
+      "-g",
+      "-DTARGET_LPC1769",
+      "-DTOOLCHAIN_GCC_ARM",
+      "-DNDEBUG",
+      "-D__CORTEX_M3",
+      "-std=gnu99",
+      "-I.",
+      "-I./LPC1769",
+      "-I./include",
+      "-I/usr/lib/gcc/arm-none-eabi/14.1.0/include",
+      "-I/usr/lib/gcc/arm-none-eabi/14.1.0/include-fixed",
+      "-I/usr/lib/gcc/arm-none-eabi/14.1.0/../../../../arm-none-eabi/include",
+      "-o",
+      "build/params.o",
+      "src/params.c"
+    ],
+    "directory": "$(pwd)",
+    "file": "$(realpath src/params.c)",
+    "output": "$(pwd)/build/params.o"
+  }
+]
 EOF
 
 mkdir -p include
